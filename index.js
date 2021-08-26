13b4174b415d791508023d736a880ea5efba9c8f
unknown <thiep@pdx.edu>
2021-08-25 18:07:13 -0700

    HTML and CSS2 and JS

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const navToggle = document.querySelector('.nav-toggle');
-
+const navItems = document.querySelectorAll('.nav-item');
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+$(document).ready(function(){
+    $("form").submit(function(){
+      alert("Submitted");
+    });
+    $("button").click(function(){
+      $("form").submit();
+    });  
+  });
